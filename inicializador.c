4dfce9e7e29d46ce039d36cf1e23197f71c81094
silviadb <chibidb@gmail.com>
2013-11-03 23:53:19 -0600

    Cambio de nombres y ordenamiento de código.

--- inicializador.c
+++ inicializador.c
@@ -1,166 +1,172 @@
 #include "common.h"
 
-int createSemaphores (int numberElements, int semaphoreKey) {
+// Funcion: crear semaforo
+//Parametros: numero elementos , key del semaforo
+int crear_sem (int num_elem, int sem_key) {
  
-printf("\n+--------------------------------------+\n");
-printf("+         SEMAPHORE CREATION           +\n");
-printf("+--------------------------------------+\n");
+	printf("\n+--------------------------------------+\n");
+	printf("+         SEMAPHORE CREATION           +\n");
+	printf("+--------------------------------------+\n");
+	 
+	int sem_ident;
+	sem_ident = semget (sem_key, 0, IPC_PRIVATE);
+	 
+	if (sem_ident < 0) {
+	 
+		 umask(0);
+		 sem_ident = semget (sem_key, NSEM,
+			  IPC_CREAT | IPC_EXCL | PERM);
+		 
+		 if (sem_ident < 0) {
+			 perror("Fallo crear array de semaforos");
+			 exit (EXIT_FAILURE);
+		 }
+		 else{
+		  	printf("> Arreglo de semaforo creado correctamente.\n");
+		 }
+		   
+		 if (semctl(sem_ident, MUTEXSEM, SETVAL, 1) > 0) {
+		 	perror("Fallo inicializar: MUTEXSEM");
+		 }
+		 else{
+		  	printf("> MUTEXSEM inicializado.\n");
+		 }
+		  
+		 if (semctl( sem_ident, EMPTYSEM,SETVAL, num_elem)< 0) {
+		 	perror("Fallo inicializar: EMPTYSEM");
+		 	exit (EXIT_FAILURE);
+		 }
+		 else{
+		  	printf("> EMPTYSEM inicializado.\n");
+		 }
+		  
+		 if (semctl( sem_ident, FULLSEM,SETVAL, 0)< 0) {
+		 	perror("Fallo inicializar: FULLSEM.");
+		 exit (EXIT_FAILURE);
+		 }
+		 else{
+		  printf("> FULLSEM inicializado.\n");
+		 }
+		 
+		 printf ("> Semaforo array {MUTEX, EMPTY, FULL} creado con ID: %d.\n"
+		  , sem_ident);
+	}
+	 
+	return sem_ident;
  
-int semaphoreIdentifier;
-semaphoreIdentifier = semget (semaphoreKey, 0, IPC_PRIVATE);
- 
-if (semaphoreIdentifier < 0) {
- 
- umask(0);
- semaphoreIdentifier = semget (semaphoreKey, NSEM,
-          IPC_CREAT | IPC_EXCL | PERM);
- 
- if (semaphoreIdentifier < 0) {
- perror("Failed to create semaphore array");
- exit (EXIT_FAILURE);
- }
- else{
-  printf("> Semaphore Array successfully creted.\n");
- }
-   
- if (semctl(semaphoreIdentifier, MUTEXSEM, SETVAL, 1) > 0) {
- perror("Failed to initialize MUTEXSEM");
- }
- else{
-  printf("> MUTEXSEM initialized.\n");
- }
-  
- if (semctl( semaphoreIdentifier, EMPTYSEM,
-      SETVAL, numberElements)< 0) {
- perror("Failed to initialize EMPTYSEM");
- exit (EXIT_FAILURE);
- }
- else{
-  printf("> EMPTYSEM initialized.\n");
- }
-  
- if (semctl( semaphoreIdentifier, FULLSEM,
-      SETVAL, 0)< 0) {
- perror("Failed to initialize FULLSEM.");
- exit (EXIT_FAILURE);
- }
- else{
-  printf("> FULLSEM initialized.\n");
- }
- 
- printf ("> Semaphore Array {MUTEX, EMPTY, FULL} created with ID: %d.\n"
-  , semaphoreIdentifier);
 }
+//-----------------------------------------------------------------------------------------
+
+// Funcion: crear_memoriacompartida
+//Parametros:key del segmento de memoria
+int crear_SharedMemory (int sharedMemory_key){
  
-return semaphoreIdentifier;
- 
+	printf("\n+--------------------------------------+\n");
+	printf("+    Asignacion de Memoria Compartida    +\n");
+	printf("+--------------------------------------+\n");
+	 
+	int sharedMemory_ident = shmget(sharedMemory_key,
+                sizeof(struct ringBuffer),
+		IPC_CREAT | PERM);
+	 
+	if (sharedMemory_ident<0) {
+	 	perror("shmget");
+	}
+	else{
+		 printf ("Memoria Compatida, ID: %d ,Tama~o: %d\n",
+		  sharedMemory_ident, (int)sizeof(struct ringBuffer));
+	}
+	return sharedMemory_ident;
 }
- 
-int createSharedMemory (int sharedMemoryKey){
- 
-printf("\n+--------------------------------------+\n");
-printf("+      SHARED MEMORY ALLOCATION        +\n");
-printf("+--------------------------------------+\n");
- 
-int sharedMemoryIdentifier = shmget(sharedMemoryKey,
-        sizeof(struct ringBuffer),
-        IPC_CREAT | PERM);
- 
-if (sharedMemoryIdentifier<0) {
- perror("shmget");
-}
-else{
- printf ("Shared Memory Slot allocated with ID: %d and Size: %d\n",
-  sharedMemoryIdentifier, (int)sizeof(struct ringBuffer));
-}
- 
-return sharedMemoryIdentifier;
-}
- 
-int permissionToProceed(int semaphoresKey, int sharedMemoryKey)
+
+//-----------------------------------------------------------------------------------------
+
+// Funcion: permiso de procesar
+//Parametros:key del semaforo, key del segmento de memoria compartida
+int permiso_procesar(int semaphoresKey, int sharedMemory_key)
 {
-int sharedMemoryIdentifier, semaphoreIdentifier, permissionToProceed;
- 
-permissionToProceed = 1;
- 
-sharedMemoryIdentifier = shmget(sharedMemoryKey, 0, 0);
-semaphoreIdentifier = semget(semaphoresKey, 0, 0);
- 
-if(sharedMemoryIdentifier > 0)
-{
- permissionToProceed--;
- printf("Shared-Memory with the given key already exists, unable to obtain shared memory.");
-}
- 
-if(semaphoreIdentifier > 0)
-{
- permissionToProceed--;
- printf("A Semaphore-Array with the given key already exists, unable to obtain Semaphore-Array.");
-}
- 
-return permissionToProceed;
+	int sharedMemory_ident, sem_ident, permiso_procesar;
+	 
+	permiso_procesar = 1;
+	 
+	sharedMemory_ident = shmget(sharedMemory_key, 0, 0);
+	sem_ident = semget(semaphoresKey, 0, 0);
+	 
+	if(sharedMemory_ident > 0)
+	{
+	 permiso_procesar--;
+	 printf("Existe el key de la memoria compartida, habilitado para obtener memoria compartida.");
+	}
+	 
+	if(sem_ident > 0)
+	{
+	 permiso_procesar--;
+	 printf("Existe el key del semafor, habilitado para obtener semaforo.");
+	}
+	 
+	return permiso_procesar;
 }
  
 int main(int argc, char **argv)
 {
  
-if(argc < 4)
- printf("Usage: resource_create <Semaphore Key> <Shared Memory Key> <Number of Products>");
-else
-{
- 
-int semaphoreKey = atoi(argv[1]);
-int sharedMemoryKey = atoi(argv[2]);
-int numberProducts = atoi(argv[3]);
- 
-int sharedMemoryIdentifier;
-char *attachedMemoryPointer;
-int semaphoreArrayIdentifier;
- 
-if(permissionToProceed(semaphoreKey, sharedMemoryKey))
-{
- 
-semaphoreArrayIdentifier = createSemaphores(numberProducts,semaphoreKey);
-sharedMemoryIdentifier = createSharedMemory(sharedMemoryKey);
- 
-int i;
-struct ringBuffer * retrieveBuffer;
- 
-if ( !(sharedMemoryIdentifier < 0 || semaphoreArrayIdentifier < 0) )
-{
- 
- retrieveBuffer = (struct ringBuffer *)shmat(sharedMemoryIdentifier, 0, 0);
- 
- if (retrieveBuffer==( struct ringBuffer *)-1)
- {
-     perror("Shmat");
- }
- else
- {
-  retrieveBuffer->size = numberProducts;
-  retrieveBuffer->isOpen = 1;
- 
-  printf("\n+--------------------------------------+\n");
-  printf("+           BUFFER INFORMATION         +\n");
-  printf("+--------------------------------------+\n");
- 
-  printf("In Pointer: %d , Out Pointer: %d\n",
-   retrieveBuffer->in, retrieveBuffer->out);
- 
-  printf("Slots: %d , Is Open: %d\n",
-   retrieveBuffer->size, retrieveBuffer->isOpen);
- 
- }
-}
-else
-{
- perror("Shmget");
-}
-}
-else
-{
- printf("The resources could not be created. Try again with new keys. \n");
-}
-}
-return EXIT_SUCCESS;
+	if(argc < 4)
+	 printf("Use: resource_create <Semaphore Key> <Shared Memory Key> <Number of Products>");
+	else
+	{
+	 
+		int sem_key = atoi(argv[1]);
+		int sharedMemory_key = atoi(argv[2]);
+		int num_prod = atoi(argv[3]);
+		 
+		int sharedMemory_ident;
+		char *attachedMemoryPointer;
+		int sem_array_ident;
+		 
+		if(permiso_procesar(sem_key, sharedMemory_key))
+		{
+		 
+			sem_array_ident = crear_sem(num_prod,sem_key);
+			sharedMemory_ident = crear_SharedMemory(sharedMemory_key);
+			 
+			int i;
+			struct ringBuffer * Aux_Buffer;
+			 
+			if ( !(sharedMemory_ident < 0 || sem_array_ident < 0) )
+			{
+			 
+				 Aux_Buffer = (struct ringBuffer *)shmat(sharedMemory_ident, 0, 0);
+				 
+				 if (Aux_Buffer==( struct ringBuffer *)-1)
+				 {
+				     perror("Shmat");
+				 }
+				 else
+				 {
+				  Aux_Buffer->size = num_prod;
+				  Aux_Buffer->isOpen = 1;
+				 
+				  printf("\n+--------------------------------------+\n");
+				  printf("+         Informacion del Buffer         +\n");
+				  printf("+--------------------------------------+\n");
+				 
+				  printf("In Puntero: %d , Out Puntero: %d\n",
+				   Aux_Buffer->in, Aux_Buffer->out);
+				 
+				  printf("Lineas: %d , Is Open: %d\n",
+				   Aux_Buffer->size, Aux_Buffer->isOpen);
+				 
+				 }
+			}
+			else
+			{
+			 	perror("Shmget");
+			}
+		}
+		else
+		{
+		 	printf("El recurso no pudo ser creado , intente de nuevo con una nueva key. \n");
+		}
+	}
+	return EXIT_SUCCESS;
 }
